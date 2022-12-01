8cb8cbf60bce451a0f536943396fbd0a54036cc8
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-12-01 23:38:43 +0500

    feat: add mock requests for service

--- src/index.js
+++ src/index.js
@@ -1,9 +1,24 @@
-const http = require('http')
+import express from 'express'
+import mongoose from 'mongoose'
+import router from './router.js'
 
-const server = http.createServer((req, res) => {
-  res.end('<h1>Hello Ken</h1>')
-})
+const port = 5000
+const DB_URL = 'mongodb+srv://ladyblaumeux24:4057321qwe@cluster0.od2acyz.mongodb.net/?retryWrites=true&w=majority'
 
-server.listen(3500, () => {
+const app = express()
 
-})
+app.use(express.json())
+app.use('/api', router)
+
+async function startApp () {
+  try {
+    await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    app.listen(port, () => {
+      console.log('server start!' + port)
+    })
+  } catch (e) {
+    console.log(e)
+  }
+}
+
+startApp()
