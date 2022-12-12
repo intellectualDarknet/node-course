a49c79a7dd02b32911d6e5fc8f764a8966f67dd9
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-12-12 18:30:21 +0500

    refactor: remove consoles comments

--- src/index.js
+++ src/index.js
@@ -10,18 +10,6 @@ const app = express()
 app.use(express.json())
 app.use('/postgres', router);
 
-// const dbStart = async () => {
-//   await db.query(`CREATE TABLE IF NOT EXISTS users (
-//     Id serial PRIMARY KEY UNIQUE,
-//     Login VARCHAR ( 50 ) UNIQUE NOT NULL,
-//     Password VARCHAR ( 50 ) NOT NULL,
-//     Age INTEGER NOT NULL,
-//     IsDeleted BOOLEAN NOT NULL,
-//     created_at VARCHAR ( 50 ),
-//     updated_at VARCHAR ( 50 )
-//   )`)
-// }
-
 (async function startApp () {
   await User.sync()
   try {
@@ -29,11 +17,6 @@ app.use('/postgres', router);
       (async () => {
         const data = JSON.parse(fs.readFileSync(path.join(__dirname, '/fakeData/fakeData.json'), 'utf-8'))
         User.bulkCreate(data)
-        // await db.query(`
-        //   INSERT INTO users (Id, Login, Password, Age, IsDeleted)
-        //   VALUES
-        //   ${data.map((obj) => `(${Object.values(obj).map((value) => typeof value === 'string' ? ('\'' + value + '\'') : value).join(', ')})`).join(', ')}
-        // `)
       })()
       console.log('server start! ' + port)
     })
