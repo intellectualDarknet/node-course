1169d0756842042512756f14fbb3ae4818bad630
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-12-12 16:17:23 +0500

    feat: add write script to insert users to table

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 const express = require('express')
 const router = require('./routes/user.routes.js')
-// const fs = require('fs')
-// const path = require('path')
-// const url = require('url')
-// const db = require('./sequelize/models/db.cjs')
+const fs = require('fs')
+const path = require('path')
 const User = require('./sequelize/models/user.cjs')
 
 const port = 8080
@@ -28,22 +26,15 @@ app.use('/postgres', router);
   await User.sync()
   try {
     app.listen(port, () => {
-      // (async () => {
-      //   const data = JSON.parse(fs.readFileSync(path.join(__dirname, '/fakeData/fakeData.json'), 'utf-8'))
-      //   await db.query(`CREATE TABLE IF NOT EXISTS users (
-      //     Id serial PRIMARY KEY UNIQUE,
-      //     Login VARCHAR ( 50 ) UNIQUE NOT NULL,
-      //     Password VARCHAR ( 50 ) NOT NULL,
-      //     Age INTEGER NOT NULL,
-      //     IsDeleted BOOLEAN NOT NULL
-      //   )`)
-      //   await db.query('DELETE FROM users')
-      //   await db.query(`
-      //     INSERT INTO users (Id, Login, Password, Age, IsDeleted)
-      //     VALUES
-      //     ${data.map((obj) => `(${Object.values(obj).map((value) => typeof value === 'string' ? ('\'' + value + '\'') : value).join(', ')})`).join(', ')}
-      //   `)
-      // })()
+      (async () => {
+        const data = JSON.parse(fs.readFileSync(path.join(__dirname, '/fakeData/fakeData.json'), 'utf-8'))
+        User.bulkCreate(data)
+        // await db.query(`
+        //   INSERT INTO users (Id, Login, Password, Age, IsDeleted)
+        //   VALUES
+        //   ${data.map((obj) => `(${Object.values(obj).map((value) => typeof value === 'string' ? ('\'' + value + '\'') : value).join(', ')})`).join(', ')}
+        // `)
+      })()
       console.log('server start! ' + port)
     })
   } catch (e) {
