15089ae797316583578b74fd0867c01ab603e8d7
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-12-20 20:39:02 +0500

    feat : add error handler function for methods

--- src/utils/handle.async.js
+++ src/utils/handle.async.js
@@ -1,6 +1,9 @@
 const func = fn => {
   return async (req, res, next) => {
-    fn(req, res, next)
+    fn(req, res, next).catch(err => {
+      res.e = err
+      next(res)
+    })
   }
 }
 module.exports = func
