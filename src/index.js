666f3f6b7d978c979927e1300469fef740137f77
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-12-20 20:02:36 +0500

    feat: complete error handling for false routes

--- src/index.js
+++ src/index.js
@@ -19,7 +19,8 @@ app.use('/postgres/users/', UsersRouter)
 
 app.all('*', (req, res, next) => {
   res.status(404).json({
-    message: ''
+    status: 'fail',
+    message: `Can't find ${req.originalUrl} on this server!`
   })
   res.e = res.e = new AppError('wrong path', 404)
   next(res)
