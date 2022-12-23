4a18240d07aae07fc02e8c47e32fd84f026cf9f2
aliaksei_dzemidovich <ladyblaumeux24@gmail.com>
2022-12-23 17:28:35 +0500

    feat: add route

--- src/controller/auth.controller.js
+++ src/controller/auth.controller.js
@@ -1,10 +1,12 @@
 const { AuthUser } = require('../models/user.model')
-const tryCatchFn = require('../error/error')
+const tryCatchFn = require('../utils/handle.async.js')
 
 class AuthController {
   signup = tryCatchFn(async (req, res, next) => {
+    res.myMethod = 'UserService createUser'
     const newUser = await AuthUser.create(req.body)
     res.status(201).json({ user: newUser })
+    next(res)
   })
 }
 
--- src/index.js
+++ src/index.js
@@ -1,84 +1,93 @@
 const express = require('express')
-
-const db = require('./db.js')
-const fs = require('fs')
-const path = require('path')
-
+const mongoose = require('mongoose')
 const UsersRouter = require('./routes/user.routes.js')
 const GroupRouter = require('./routes/group.router.js')
 const AuthRouter = require('./routes/auth.router.js')
-const { User } = require('./sequelize/models')
-const logger = require('./logger/logger')
-
 const wrongPathHandler = require('./controller/path.controller.js')
 const generalHandler = require('./controller/general.controller.js')
 
-const port = 8080
+const port = 5000
+const DB_URL = 'mongodb+srv://ladyblaumeux24:4057321qwe@cluster0.od2acyz.mongodb.net/?retryWrites=true&w=majority'
+
 const app = express()
 
-process.on('uncaughtException', err => {
-  logger.error(`${err.name} ${err.message}`)
-  logger.inform('UNCAUGHT EXCEPTION! Shutting down...')
-  process.exit(1)
-})
-
 app.use(express.json())
-// app.use((req, res, next) => {
-//   console.log(b)
-//   next()
-// })
-app.use('/postres/user/', AuthRouter)
+app.use('/postgres/groups/', AuthRouter)
 app.use('/postgres/groups/', GroupRouter)
 app.use('/postgres/users/', UsersRouter)
 
 app.all('*', wrongPathHandler)
 app.use(generalHandler)
-let server;
 
-(async function startApp () {
+async function startApp () {
   try {
-    await db.sync({ force: true })
-    server = app.listen(port, () => {
-      (async () => {
-        const data = JSON.parse(fs.readFileSync(path.join(__dirname, '/fakeData/fakeData.json'), 'utf-8'))
-        User.bulkCreate(data)
-      })()
-      console.log('server start! ' + port)
+    await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
+    app.listen(port, () => {
+      console.log('server start!' + port)
     })
   } catch (e) {
     console.log(e)
   }
-})()
+}
 
-process.on('unhandledRejection', err => {
-  logger.error(`${err.name} ${err.message}`)
-  logger.inform('UNHANDLER REJECTION! Shutting down...')
-  server.close(() => {
-    process.exit(1)
-  })
-})
+startApp()
 
-// import express from 'express'
-// import mongoose from 'mongoose'
-// import router from './router.js'
+// const express = require('express')
 
-// const port = 5000
-// const DB_URL = 'mongodb+srv://ladyblaumeux24:4057321qwe@cluster0.od2acyz.mongodb.net/?retryWrites=true&w=majority'
+// const db = require('./db.js')
+// const fs = require('fs')
+// const path = require('path')
 
+// const UsersRouter = require('./routes/user.routes.js')
+// const GroupRouter = require('./routes/group.router.js')
+// const AuthRouter = require('./routes/auth.router.js')
+// const { User } = require('./sequelize/models')
+// const logger = require('./logger/logger')
+
+// const wrongPathHandler = require('./controller/path.controller.js')
+// const generalHandler = require('./controller/general.controller.js')
+
+// const port = 8080
 // const app = express()
 
-// app.use(express.json())
-// app.use('/api', router)
+// process.on('uncaughtException', err => {
+//   logger.error(`${err.name} ${err.message}`)
+//   logger.inform('UNCAUGHT EXCEPTION! Shutting down...')
+//   process.exit(1)
+// })
 
-// async function startApp () {
+// app.use(express.json())
+// // app.use((req, res, next) => {
+// //   console.log(b)
+// //   next()
+// // })
+// app.use('/postres/user/', AuthRouter)
+// app.use('/postgres/groups/', GroupRouter)
+// app.use('/postgres/users/', UsersRouter)
+
+// app.all('*', wrongPathHandler)
+// app.use(generalHandler)
+// let server;
+
+// (async function startApp () {
 //   try {
-//     await mongoose.connect(DB_URL, { useUnifiedTopology: true, useNewUrlParser: true })
-//     app.listen(port, () => {
-//       console.log('server start!' + port)
+//     await db.sync({ force: true })
+//     server = app.listen(port, () => {
+//       (async () => {
+//         const data = JSON.parse(fs.readFileSync(path.join(__dirname, '/fakeData/fakeData.json'), 'utf-8'))
+//         User.bulkCreate(data)
+//       })()
+//       console.log('server start! ' + port)
 //     })
 //   } catch (e) {
 //     console.log(e)
 //   }
-// }
+// })()
 
-// startApp()
+// process.on('unhandledRejection', err => {
+//   logger.error(`${err.name} ${err.message}`)
+//   logger.inform('UNHANDLER REJECTION! Shutting down...')
+//   server.close(() => {
+//     process.exit(1)
+//   })
+// })
